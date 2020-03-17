c74c4a9d9748d97a5accab1c3c512357ec0dd709
zhugequn <hzzhugequn@corp.netease.com>
2020-03-17 13:07:56 +0800

    listnode工具类

---
+++ src/main/java/com/francis/leetcode/utils/ListNodeUtils.java
@@ -0,0 +1,35 @@
+package com.francis.leetcode.utils;
+
+import com.francis.leetcode.utils.entity.ListNode;
+
+/**
+ * @author zhugequn
+ * @since jdk8
+ */
+public class ListNodeUtils {
+
+    public static ListNode createList(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return null;
+        ListNode head = new ListNode(arr[0]);
+        ListNode cur = head;
+        for (int i = 1; i < arr.length; i++) {
+            ListNode node = new ListNode(arr[i]);
+            cur.next = node;
+            cur = node;
+        }
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            if (current.next == null)
+                System.out.print(current.val);
+            else
+                System.out.print(current.val + "->");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/francis/leetcode/utils/entity/ListNode.java
@@ -0,0 +1,22 @@
+package com.francis.leetcode.utils.entity;
+
+/**
+ * @author zhugequn
+ * @since jdk8
+ */
+public class ListNode {
+
+    /**
+     * 值
+     */
+    public int val;
+
+    /**
+     * next指针
+     */
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
