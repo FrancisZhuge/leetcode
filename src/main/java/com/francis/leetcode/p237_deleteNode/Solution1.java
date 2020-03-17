75f2ce9814193235be6687d29b8091a8b81457fa
zhugequn <hzzhugequn@corp.netease.com>
2020-03-17 22:06:16 +0800

    删除链表中的节点

--- src/main/java/com/francis/leetcode/p237_deleteNode/Solution1.java
+++ src/main/java/com/francis/leetcode/p237_deleteNode/Solution1.java
@@ -1,6 +1,8 @@
 package com.francis.leetcode.p237_deleteNode;
 
+import com.francis.leetcode.utils.ListNodeUtils;
 import com.francis.leetcode.utils.entity.ListNode;
+import org.junit.Test;
 
 /**
  * @author zhugequn
@@ -11,5 +13,13 @@ public class Solution1 {
     public void deleteNode(ListNode node) {
         node.val = node.next.val;
         node.next = node.next.next;
+    }
+
+    @Test
+    public void test(){
+        ListNode head = ListNodeUtils.createList(new int[]{4, 5, 1, 9});
+        ListNodeUtils.printList(head);
+        deleteNode(ListNodeUtils.findList(head, 1));
+        ListNodeUtils.printList(head);
     }
 }
