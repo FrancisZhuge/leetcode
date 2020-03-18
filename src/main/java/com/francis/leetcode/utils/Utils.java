9300417cf9c37598539f007972391b47daf23a6f
zhugequn <hzzhugequn@corp.netease.com>
2020-03-18 20:05:39 +0800

    更新树工具类

---
+++ src/main/java/com/francis/leetcode/utils/Utils.java
@@ -0,0 +1,122 @@
+package com.francis.leetcode.utils;
+
+import com.francis.leetcode.utils.entity.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author zhugequn
+ * @since jdk8
+ */
+public class Utils {
+
+    public static TreeNode createTree(Integer[] arr) {
+        return createTree(arr, 0);
+    }
+
+    private static TreeNode createTree(Integer[] arr, int index) {
+        TreeNode node = null;
+        if (index < arr.length) {
+            Integer val = arr[index];
+            if (val != null) {
+                node = new TreeNode(val);
+                node.left = createTree(arr, index * 2 + 1);
+                node.right = createTree(arr, index * 2 + 2);
+            }
+        }
+        return node;
+    }
+
+    public static void printTree(TreeNode root) {
+        int maxLevel = maxLevel(root);
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static void printNodeInternal(List<TreeNode> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || isAllElementsNull(nodes))
+            return;
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+        printWhitespaces(firstSpaces);
+        List<TreeNode> newNodes = new ArrayList<TreeNode>();
+        for (TreeNode node : nodes) {
+            if (node != null) {
+                System.out.print(node.val);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+            printWhitespaces(betweenSpaces);
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+                if (nodes.get(j).left != null)
+                    System.out.print("/");
+                else
+                    printWhitespaces(1);
+                printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).right != null)
+                    System.out.print("\\");
+                else
+                    printWhitespaces(1);
+                printWhitespaces(endgeLines + endgeLines - i);
+            }
+            System.out.println("");
+        }
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+
+    private static <T extends Comparable<?>> int maxLevel(TreeNode node) {
+        if (node == null)
+            return 0;
+        return Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+    }
+
+    private static <T> boolean isAllElementsNull(List<T> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
+        return true;
+    }
+
+    public static void print2Arrays(int[][] arrays) {
+        for (int i = 0; i < arrays.length; i++) {
+            for (int j = 0; j < arrays[0].length; j++) {
+                System.out.print(arrays[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void print2Arrays(char[][] arrays) {
+        for (int i = 0; i < arrays.length; i++) {
+            for (int j = 0; j < arrays[0].length; j++) {
+                System.out.print(arrays[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
