7422b2f588198dad7778cb8b9d0c94a9e8e4357d
zhugequn <hzzhugequn@corp.netease.com>
2020-03-18 13:03:52 +0800

    有效的括号

---
+++ src/main/java/com/francis/leetcode/p20_isValid/Solution1.java
@@ -0,0 +1,37 @@
+package com.francis.leetcode.p20_isValid;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+/**
+ * @author zhugequn
+ * @since jdk8
+ */
+public class Solution1 {
+
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) return true;
+        int n = s.length();
+        if (n % 2 == 1) return false;
+
+        Stack<Character> record = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') record.push(c);
+            else {
+                if (record.isEmpty()) return false;
+                char match;
+                if (c == ')') match = '(';
+                else if (c == '}') match = '{';
+                else match = '[';
+                if (match != record.pop()) return false;
+            }
+        }
+        return record.isEmpty();
+    }
+
+    @Test
+    public void test() {
+        System.out.println(isValid("()"));
+    }
+}
