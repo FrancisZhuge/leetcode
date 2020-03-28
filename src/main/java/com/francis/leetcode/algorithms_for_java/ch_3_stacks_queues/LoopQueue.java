70484185657e4c6b37c90239d90894003ec2c6a1
zhugequn <hzzhugequn@corp.netease.com>
2020-03-28 22:27:59 +0800

    队列循环实现

---
+++ src/main/java/com/francis/leetcode/algorithms_for_java/ch_3_stacks_queues/LoopQueue.java
@@ -0,0 +1,87 @@
+package com.francis.leetcode.algorithms_for_java.ch_3_stacks_queues;
+
+/**
+ * 循环队列
+ * front == tail 队列为空
+ * (tail+1)%size == front 队列满
+ *
+ * @author zhugequn
+ * @since jdk8
+ */
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+
+    private int front;
+
+    private int tail;
+
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public LoopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if ((tail + 1) % data.length == front)
+            resize(getCapacity() * 2);
+
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size++;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity + 1];
+        for (int i = 0; i < size; i++)
+            newData[i] = data[(i + front) % data.length];
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty())
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
+
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0)
+            resize(getCapacity() / 2);
+        return ret;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty())
+            throw new IllegalArgumentException("Queue is empty.");
+        return data[front];
+    }
+}
