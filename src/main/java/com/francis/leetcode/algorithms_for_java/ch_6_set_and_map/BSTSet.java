bf214036e931ec0fe67e3aa142b31f32c63e2286
zhugequn <hzzhugequn@corp.netease.com>
2020-03-30 22:31:41 +0800

    二分搜索树集合

---
+++ src/main/java/com/francis/leetcode/algorithms_for_java/ch_6_set_and_map/BSTSet.java
@@ -0,0 +1,41 @@
+package com.francis.leetcode.algorithms_for_java.ch_6_set_and_map;
+
+import com.francis.leetcode.algorithms_for_java.ch_5_binary_search_tree.BST;
+
+/**
+ * @author zhugequn
+ * @since jdk8
+ */
+public class BSTSet<E extends Comparable<E>> implements Set<E> {
+
+    private BST<E> bst;
+
+    public BSTSet() {
+        bst = new BST<>();
+    }
+
+    @Override
+    public int getSize() {
+        return bst.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return bst.isEmpty();
+    }
+
+    @Override
+    public void add(E e) {
+        bst.add(e);
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return bst.contains(e);
+    }
+
+    @Override
+    public void remove(E e) {
+        bst.remove(e);
+    }
+}
