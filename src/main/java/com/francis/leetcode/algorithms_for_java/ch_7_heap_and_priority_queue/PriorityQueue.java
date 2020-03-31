922ffc8b6112b0188224d2a46e3f683ff2377262
zhugequn <hzzhugequn@corp.netease.com>
2020-03-31 21:39:28 +0800

    优先队列

---
+++ src/main/java/com/francis/leetcode/algorithms_for_java/ch_7_heap_and_priority_queue/PriorityQueue.java
@@ -0,0 +1,41 @@
+package com.francis.leetcode.algorithms_for_java.ch_7_heap_and_priority_queue;
+
+import com.francis.leetcode.algorithms_for_java.ch_3_stacks_queues.Queue;
+
+/**
+ * @author zhugequn
+ * @since jdk8
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue() {
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+}
